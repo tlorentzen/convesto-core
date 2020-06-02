23b9d60a8018983eaf26441d330cfeddbc85f5ba
Thomas Lorentzen <thomas@tlorentzen.net>
2020-06-02 17:13:04 +0200

    Added user core

--- src/CoreUser.php
+++ src/CoreUser.php
@@ -1,10 +1,38 @@
 <?php
 namespace convesto\core\Models;
 
-use Illuminate\Support\Facades\Log;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-abstract class CoreUser extends Model
+abstract class CoreUser extends Authenticatable
 {
+    use Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
